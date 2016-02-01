4c4f8cd0db4758c9ad2053aa336944e92658056d
Bjørn Tower <bjornsnorgaard@gmail.com>
2016-02-02 00:25:14 +0100

    done with ex1

--- 01 Calculator/Calculater/Application/Program.cs
+++ 01 Calculator/Calculater/Application/Program.cs
@@ -1,23 +1,44 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Calculater_namespace;
 
 namespace Application
 {
     class Program
     {
+        static Calculator _calculator = new Calculator();
+
         static void Main(string[] args)
         {
             Console.WriteLine("Commence testing!");
 
             TestAdd();
+            TestSubtract();
+            TestMultiply();
+            TestExponential();
         }
 
         private static void TestAdd()
         {
+            Console.Write("Adding... \t");
+            Console.WriteLine(_calculator.Add(1, 1) == 2 ? "Works!" : "Does NOT work!");
+        }
 
+        private static void TestSubtract()
+        {
+            Console.Write("Subtracting... \t");
+            Console.WriteLine(_calculator.Subtract(10, 4) == 6 ? "Works!" : "Does NOT work!");
+        }
+
+        private static void TestMultiply()
+        {
+            Console.Write("Multiplying... \t");
+            Console.WriteLine(_calculator.Multiply(2, 3) == 6 ? "Works!" : "Does NOT work!");
+        }
+
+        private static void TestExponential()
+        {
+            Console.Write("Power... \t");
+            Console.WriteLine(_calculator.Power(3, 2) == 9 ? "Works!" : "Does NOT work!");
         }
     }
 }
