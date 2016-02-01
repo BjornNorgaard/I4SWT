4816da211221227d0c0f85e956a7aec8e645a9c7
Bjørn Tower <bjornsnorgaard@gmail.com>
2016-02-01 22:24:05 +0100

    made calculator, needs testing

---
+++ 01 Calculator/Calculater/Calculater/Calculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Calculater_namespace
+{
+    public class Calculator
+    {
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+        public double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+        public double Multiply(double a, double b)
+        {
+            return a*b;
+        }
+        public double Power(double x, double exp)
+        {
+            return Math.Pow(x, exp);
+        }
+    }
+}
