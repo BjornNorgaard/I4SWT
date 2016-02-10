b0f7e56443f45deea52c7960818d6b37b6af5679
Bjørn Tower <bjornsnorgaard@gmail.com>
2016-02-10 19:40:07 +0100

    Added test project and references

---
+++ HelloJenkins/Calculator.Test.Unit/CalculatorUnitTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Calculator.Test.Unit
+{
+    [TestFixture]
+    public class CalculatorUnitTest
+    {
+        [Test]
+        public void Add_AddTwoNumbers_SumReturned()
+        {
+            var uut = new Calc();
+            Assert.That(uut.Add(2, 3), Is.EqualTo(5));
+        }
+    }
+}
