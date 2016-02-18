43108ccf999a3e7f6c001ff860f5192741d6a53e
Nørgaard Laptop VS <Bjornsnorgaard@gmail.com>
2016-02-18 11:38:23 +0100

    added 2n exercise for Jonas

---
+++ Andengradsligning/Andengradsligning.Test.Unit/AndengradsligningTestUnit.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exceptions;
+using NUnit.Framework;
+
+namespace Andengradsligning.Test.Unit
+{
+    [TestFixture]
+    public class AndengradsligningTestUnit
+    {
+        private Andengradsligning _uut;
+
+        [SetUp]
+        public void Setup()
+        {
+            _uut = new Andengradsligning();
+        }
+
+        [TestCase(5, 0, -5, 1)]
+        [TestCase(-5, 2, 1, -0.28989794855663559d)]
+        public void FindRoots_FindCorrectRoots_SetsMemberR1(int a, int b, int c, double d)
+        {
+            _uut.FindRoots(a, b, c);
+            Assert.That(_uut.R1, Is.EqualTo(d));
+        }
+
+        [Test]
+        public void FindRoots_FindFaultyRoots_ThrowsExcep()
+        {
+            Assert.Throws<NoRootsException>(() => _uut.FindRoots(5, 2, 1));
+        }
+    }
+}
---
+++ Andengradsligning/Andengradsligning/Andengradsligning.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exceptions;
+
+namespace Andengradsligning
+{
+    public class Andengradsligning
+    {
+        public double R1 { get; private set; } = 0;
+        public double R2 { get; private set; } = 0;
+        public double D { get; set; }
+
+        public void FindRoots(int a, int b, int c)
+        {
+             D = b * b - 4 * a * c;
+
+            if (D == 0)
+            {
+                R1 = (-b + Math.Sqrt(D))/(a*2);
+            }
+            else if (D > 0)
+            {
+                R1 = (-b + Math.Sqrt(D)) / (a * 2);
+                R2 = (-b - Math.Sqrt(D)) / (a * 2);
+            }
+            else if (D < 0)
+            {
+                throw new NoRootsException();
+            }
+        }
+    }
+}
