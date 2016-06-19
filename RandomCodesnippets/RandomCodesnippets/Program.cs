3f61ea82a623f865d738c6b8e79c3a23d4cdd9f4
Norgaard Tower <bjornsnorgaard@gmail.com>
2016-06-19 23:05:15 +0200

    minor chagnes

--- RandomCodesnippets/RandomCodesnippets/Program.cs
+++ RandomCodesnippets/RandomCodesnippets/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +13,34 @@ namespace RandomCodesnippets
     {
         static void Main(string[] args)
         {
+            //var x = 2;
+
+            //while (x == 2)
+            //{
+            //    Console.Write("herro");
+            //}
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            
+            if (a == 10)
+            {
+            }
+
+            Foo(a, b);
+        }
+
+        static void Foo(int x, int y)
+        {
+            switch (x)
+            {
+                case 1:
+                    Console.WriteLine(y);
+                    break;
+                default:
+                    Console.WriteLine(y);
+                    break;
+            }
         }
     }
 }
