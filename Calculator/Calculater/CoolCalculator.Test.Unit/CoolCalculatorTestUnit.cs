81ff88b101de61bb221ef459d62c296bb8ec613e
Norgaard Tower <bjornsnorgaard@gmail.com>
2016-06-19 14:21:48 +0200

    added example for stub also

--- Calculator/Calculater/CoolCalculator.Test.Unit/CoolCalculatorTestUnit.cs
+++ Calculator/Calculater/CoolCalculator.Test.Unit/CoolCalculatorTestUnit.cs
@@ -27,5 +27,12 @@ namespace CoolCalculator.Test.Unit
             _uut.Add(1,2);
             _fakeAdditionCalculator.Received().Add(1, Arg.Any<int>());
         }
+
+        [Test]
+        public void Add_Adding1And2_Returns3()
+        {
+            _fakeAdditionCalculator.Add(1, 2).Returns(3);
+            Assert.That(_uut.Add(1, 2), Is.EqualTo(3));
+        }
     }
 }
